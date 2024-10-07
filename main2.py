fb7a0fa8f4fc9b4d1948cf651e4bfe976c7d9b4f
lorenzoFOR1 <lorenzo.f1@aluno.ifsc.edu.br>
2024-10-07 13:12:11 -0300

    equacaocalculator

--- main2.py
+++ main2.py
@@ -1,6 +1,21 @@
 #coding: utf-8
 
+import math as mt
+'''
 #true
 print(5!=7)
 #false
 print(7!=7)
+'''
+
+def eq2grau(a,b,c):
+    Delta = mt.pow(b,2)-4*a*c
+    if (Delta>0):
+        #duas reais distintas
+        x1 = (-b+pow(Delta, 0.5))/(2*a)
+        x2 = (-b-pow(Delta, 0.5))/(2*a)
+        return x1,x2
+
+if __name__=="__main__":
+    x1,x2 = eq2grau(1,4,3)
+    print(f"as raizes sao {x1} e {x2}")
