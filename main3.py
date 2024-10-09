164225673ea319c5e82020b6f2ef31e38d6ef1aa
lorenzoFOR1 <lorenzo.f1@aluno.ifsc.edu.br>
2024-10-09 13:07:49 -0300

    beecrowd

--- main3.py
+++ main3.py
@@ -1,1 +1,13 @@
 #coding: utf-8
+
+import math as mt
+
+notaa = float(input())
+notab = float(input())
+notac = float(input())
+peso1 = 2
+peso2 = 3
+peso3 = 5
+media_ponderada = (notaa*2+notab*3+notac*5)/(2+3+5)
+saida = "MEDIA = {0:.1f}".format(media_ponderada)
+print(saida)
