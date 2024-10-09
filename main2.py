0899671fd13e5337a38336969ffc3964bd9d6de2
lorenzoFOR1 <lorenzo.f1@aluno.ifsc.edu.br>
2024-10-09 12:11:01 -0300

    equacaocalculatorv3

--- main2.py
+++ main2.py
@@ -10,18 +10,25 @@ print(7!=7)
 
 def eq2grau(a,b,c):
     Delta = mt.pow(b,2)-4*a*c
+    x1 = None
+    x2 = None
     if (Delta>0):
         #duas reais distintas
         x1 = (-b-pow(Delta, 0.5))/(2*a)
         x2 = (-b+pow(Delta, 0.5))/(2*a)
-        return x1,x2
+
     elif(Delta == 0):
         print("Há raiz unica, Delta = ", Delta)
-        return Delta
+        
     else:
         print("Raizes complexas, Delta = ", Delta)
-        return Delta
 
+    return x1,x2, Delta
+        
 if __name__=="__main__":
-    x1,x2 = eq2grau(1,4,3)
-    print(f"as raizes sao {x1} e {x2}")
+    saida = eq2grau(1,4,4)
+    print(saida)
+    print("qtd de termos:", len(saida))
+    print("primeiro termo", saida[0])
+    print("segundo termo", saida[1])
+    print("terceiro termo", saida[2])
