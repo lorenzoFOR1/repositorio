49eb91af68e5a1db4e72561cc047d8c970c2e14d
lorenzoFOR1 <lorenzo.f1@aluno.ifsc.edu.br>
2024-10-11 12:55:35 -0300

    while

---
+++ while.py
@@ -0,0 +1,12 @@
+#coding: utf-8
+
+numero = int(float(input("entre com numero menor do que 10:")))
+contador = 0
+while numero<20:
+    numero+=1
+    print(numero)
+    contador+=1
+else:
+    print(f"Qde termos: {contador}")    
+
+
