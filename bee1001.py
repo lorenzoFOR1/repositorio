2aad258f5ddbaa41d37f857a92a6b8fafd085280
lorenzoFOR1 <lorenzo.f1@aluno.ifsc.edu.br>
2024-10-11 12:54:58 -0300

    beecrowd

---
+++ bee1001.py
@@ -0,0 +1,8 @@
+#coding: utf-8
+
+import math as mt
+
+A = int(input())
+B = int(input())
+X = A + B
+print("X =", X)
---
+++ bee1002.py
@@ -0,0 +1,8 @@
+#coding: utf-8
+
+import math as mt
+
+R = int(float(input()))
+Pi = 3.1415
+A =  Pi*(R*R)
+print("A=", A)
