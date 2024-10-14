c2f03dcfcbc888276c973e0f3e19e7e2acbc742a
lorenzoFOR1 <lorenzo.f1@aluno.ifsc.edu.br>
2024-10-14 12:38:22 -0300

    if

--- vetoresemojidechoro.py
+++ vetoresemojidechoro.py
@@ -8,7 +8,14 @@ def script0():
     print(vA)
     print(vB)
     print(vC)
-    print("batman aparece", vC.count("batman"),"vez")
+    print("batman aparece", vC.count("batman"),"vez(es)")
+
+def script1in():
+    bebidas = ["coco-cola", "pespi", "guarana", "leite"]
+    print("coco-cola" in bebidas)
+    print("vodka" not in bebidas)#vodka not in = true se vodka in bebidas = false
+    
 
 if __name__=="__main__":
-    script0()
+    #script0()
+    script1in()
