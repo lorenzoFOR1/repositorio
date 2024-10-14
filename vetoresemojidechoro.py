c7cac8551f91105ae0f7ffef52358bf39be8532f
lorenzoFOR1 <lorenzo.f1@aluno.ifsc.edu.br>
2024-10-14 12:52:27 -0300

    topbaresonlineusandoif

--- vetoresemojidechoro.py
+++ vetoresemojidechoro.py
@@ -13,7 +13,19 @@ def script0():
 def script1in():
     bebidas = ["coco-cola", "pespi", "guarana", "leite"]
     print("coco-cola" in bebidas)
-    print("vodka" not in bebidas)#vodka not in = true se vodka in bebidas = false
+    print("vodka" not in bebidas)#vodka not in = true , vodka in bebidas = false
+    print("lista de bebidas:", bebidas)
+    bebida_gelada = "dolly"
+    if (bebida_gelada not in bebidas):
+        prompt = input("deseja adicionar uma bebida?:")
+        bebidas.append(bebida_gelada)
+        print(f"{bebida_gelada} adicionada ao vetor")
+    else:
+        print(f"{bebida_gelada} já está no vetor")
+    print(bebidas)
+
+    #if (bebida_gelada in bebidas):
+        #print(f"{bebida_gelada} já está no vetor")
     
 
 if __name__=="__main__":
